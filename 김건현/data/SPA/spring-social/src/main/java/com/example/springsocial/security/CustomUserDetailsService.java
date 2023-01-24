2a52a276a8113010c9c253791e4f19fa463758e9
gggeon <39759666+gggeon96@users.noreply.github.com>
2023-01-25 00:30:01 +0900

    Docs: 참고자료 추가

---
+++ 김건현/data/SPA/spring-social/src/main/java/com/example/springsocial/security/CustomUserDetailsService.java
@@ -0,0 +1,46 @@
+package com.example.springsocial.security;
+
+
+import com.example.springsocial.exception.ResourceNotFoundException;
+import com.example.springsocial.model.User;
+import com.example.springsocial.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by rajeevkumarsingh on 02/08/17.
+ */
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    //userRepository에서 email로 가져오기
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with email : " + email)
+        );
+
+        return UserPrincipal.create(user);
+    }
+
+    @Transactional
+    //userRepository에서 id값으로 가져오기
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(
+            () -> new ResourceNotFoundException("User", "id", id)
+        );
+
+        return UserPrincipal.create(user);
+    }
+}
---
+++ 김건현/data/SPA/spring-social/src/test/java/com/example/springsocial/SpringSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringSocialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
