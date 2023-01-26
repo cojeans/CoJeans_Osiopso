6efa9695482d7fc16f33e264303a922a4f4ee6ad
bulnabang99 <bulnabang99@gmail.com>
2023-01-26 22:43:34 +0900

    Fix: swagger dependecny 추가

--- osiopso/src/main/java/com/cojeans/osiopso/api/controller/FeedApiController.java
+++ osiopso/src/main/java/com/cojeans/osiopso/api/controller/FeedApiController.java
@@ -2,6 +2,7 @@ package com.cojeans.osiopso.api.controller;
 
 import com.cojeans.osiopso.entity.feed.Article;
 import com.cojeans.osiopso.service.ArticleService;
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -17,6 +18,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/feed")
+@Api(tags = "게시글 관련 API")
 public class FeedApiController {
 
     private final ArticleService articleService;
