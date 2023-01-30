dce9139b8c31540e1e037b5f7cad81a1c52b25b0
SSAFY <youngseokgo26@naver.com>
2023-01-30 15:20:18 +0900

    S08P12C106-180 Docs:home.component.jsx

--- osiopso-front/src/routes/home/home.component.jsx
+++ osiopso-front/src/routes/home/home.component.jsx
@@ -1,9 +1,21 @@
 import './home.styles'
+import { TextToCenter, TextToLeft, Category  } from './home.styles'
 
-const Home = () => {
+const Home = () =>{
 	return (
 		<div>
-			<h1>Home</h1>
+			<TextToCenter><h2>Home</h2></TextToCenter>
+			<TextToCenter>#태그가 들어올 자리</TextToCenter>
+			
+
+			<TextToLeft><h4>와글와글 토론장</h4></TextToLeft>
+
+
+			<hr/>
+			<Category><h4>최신순</h4> <h4>인기순</h4> <h4>논란순</h4></Category>
+			
+
+
 		</div>
 	)
 }
--- osiopso-front/src/routes/home/home.styles.jsx
+++ osiopso-front/src/routes/home/home.styles.jsx
@@ -1,1 +1,17 @@
 import styled from 'styled-components'
+
+
+
+export const TextToCenter = styled.div`
+text-align: center;
+`
+export const Category = styled.div`
+text-align: center;
+display: grid;
+grid-template-columns: 1fr 1fr 1fr;
+margin: 0px 20px 0px 
+`
+export const TextToLeft = styled.div`
+text-align: left;
+`
+
