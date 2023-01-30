f8879ba0154c092e289ac1396c7dd31788eaeda8
bulnabang99 <bulnabang99@gmail.com>
2023-01-31 00:43:59 +0900

    S08P12C106-110 Feat: OotdRepository 추가

--- osiopso/src/main/java/com/cojeans/osiopso/repository/article/OotdRepository.java
+++ osiopso/src/main/java/com/cojeans/osiopso/repository/article/OotdRepository.java
@@ -1,4 +1,17 @@
-package com.cojeans.osiopso.repository;
+package com.cojeans.osiopso.repository.article;
 
-public interface OotdRepository {
+import com.cojeans.osiopso.entity.feed.Article;
+import com.cojeans.osiopso.entity.feed.Ootd;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface OotdRepository extends JpaRepository<Ootd, Long> {
+
+    // 피드 이미지 로드
+//    @Query("select a from Article a join ArticlePhoto ap on a.id = ap.article.id " +
+//            "where a.dtype = 'O'")
+    @Query("select a from Article a where a.dtype = 'O'")
+    List<Ootd> findList();
 }
