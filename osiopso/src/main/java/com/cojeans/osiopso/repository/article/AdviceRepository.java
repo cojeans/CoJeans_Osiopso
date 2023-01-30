0caad6ef5b1cf223a93d64298d00394e77be41a3
bulnabang99 <bulnabang99@gmail.com>
2023-01-31 00:46:16 +0900

    S08P12C106-110 Refactor: 기능 분리설계

---
+++ osiopso/src/main/java/com/cojeans/osiopso/repository/article/AdviceRepository.java
@@ -0,0 +1,14 @@
+package com.cojeans.osiopso.repository.article;
+
+import com.cojeans.osiopso.entity.feed.Advice;
+import com.cojeans.osiopso.entity.feed.Ootd;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface AdviceRepository extends JpaRepository<Advice, Long> {
+
+    @Query("select a from Article a where a.dtype = 'A'")
+    List<Advice> findList();
+}
