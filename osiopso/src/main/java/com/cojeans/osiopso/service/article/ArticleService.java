2305f7bf8a1207bb2a4f04235a3463b5d2ad1f90
bulnabang99 <bulnabang99@gmail.com>
2023-01-31 23:52:57 +0900

    Feat: 게시물 삭제시 연관된 태그도 함께 삭제, 게시물 작성시 태그들도 함께 저장

--- osiopso/src/main/java/com/cojeans/osiopso/service/article/ArticleService.java
+++ osiopso/src/main/java/com/cojeans/osiopso/service/article/ArticleService.java
@@ -3,10 +3,12 @@ package com.cojeans.osiopso.service.article;
 import com.cojeans.osiopso.dto.feed.ArticleDto;
 import com.cojeans.osiopso.dto.feed.ArticlePhotoDto;
 import com.cojeans.osiopso.dto.feed.ArticleTagDto;
+import com.cojeans.osiopso.dto.feed.TagDto;
 import com.cojeans.osiopso.entity.feed.Advice;
 import com.cojeans.osiopso.entity.feed.Article;
 import com.cojeans.osiopso.entity.feed.ArticlePhoto;
 import com.cojeans.osiopso.entity.feed.ArticleTag;
+import com.cojeans.osiopso.entity.tag.Tag;
 import com.cojeans.osiopso.entity.user.User;
 import com.cojeans.osiopso.repository.article.*;
 import lombok.RequiredArgsConstructor;
@@ -30,23 +32,53 @@ public class ArticleService {
         // 추후에 로그인 기능이 완성된다면 어떤식으로 유저 정보(JWT) 를 받아올지?
         User token = new User();
 
-        articleRepository.save(articleDto.toEntity(token, 0L));
-        tagRepository.save(articleDto.toTagEntity());
-        articleTagRepository.save(articleDto.toArticleTag());
+        List<TagDto> tags = articleDto.getTags();
+        Article article = articleDto.toEntity(token, 0L);
+
+        // 1. 게시물 먼저 저장
+        // 2. Dto 필드 중, 태그의 개수만큼 태그 저장
+        // 3. ArticleTag 에 게시물과 유저를 넣고 저장
+        articleRepository.save(article);
+
+        for (TagDto tag : tags) {
+            Tag tagE = tag.toTagEntity();
+            Tag tagSaved = tagRepository.save(tagE);
 
 
+            ArticleTag articleTagE = ArticleTag.builder()
+                    .article(article)
+                    .tag(tagSaved)
+                    .build();
 
+            articleTagRepository.save(articleTagE);
+        }
 
-        return true;
+//        return true;
 //        if (articleRepository.save(articleDto.toEntity(token, 0L)) == null) {
 //            return false;
 //        } else {
 //            return true;
 //        }
+        return true;
     }
 
     public boolean deleteArticle(Long articleNo) {
-        articleRepository.deleteById(articleNo);
+
+        // 1번 article을 지울 때..
+        // 1번 article을 외래키로 가진 article_tag 조회
+        // 해당 article_tag 의 tag_id를 찾아서 tag 삭제
+        // 해당 article_tag 삭제
+        // 게시물 삭제
+
+        List<ArticleTag> articleTag = articleTagRepository.findByArticle_Id(articleNo);
+        for (ArticleTag at : articleTag) {
+            articleTagRepository.deleteById(at.getId());
+            tagRepository.deleteById(at.getTag().getId());
+        }
+
+        Long articleId = articleRepository.findById(articleNo).orElseThrow().getId();
+        articleRepository.deleteById(articleId);
+
 
         Optional<Article> byId = articleRepository.findById(articleNo);
         System.out.println(byId);
