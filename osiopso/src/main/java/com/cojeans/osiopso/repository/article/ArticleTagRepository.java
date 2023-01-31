07cba09813d657be1483a19e15ae357cc0ac5189
bulnabang99 <bulnabang99@gmail.com>
2023-01-31 23:50:57 +0900

    Feat: findByArticle_Id 추가

--- osiopso/src/main/java/com/cojeans/osiopso/repository/article/ArticleTagRepository.java
+++ osiopso/src/main/java/com/cojeans/osiopso/repository/article/ArticleTagRepository.java
@@ -3,5 +3,8 @@ package com.cojeans.osiopso.repository.article;
 import com.cojeans.osiopso.entity.feed.ArticleTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ArticleTagRepository extends JpaRepository<ArticleTag, Long> {
+    List<ArticleTag> findByArticle_Id(Long articleNo);
 }
