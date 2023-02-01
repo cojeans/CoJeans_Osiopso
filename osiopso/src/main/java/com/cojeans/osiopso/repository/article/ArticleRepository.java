180385169cb70abf1966fd6aa165c94bc4538ac1
bulnabang99 <bulnabang99@gmail.com>
2023-02-02 00:05:43 +0900

    Style: none

--- osiopso/src/main/java/com/cojeans/osiopso/repository/article/ArticleRepository.java
+++ osiopso/src/main/java/com/cojeans/osiopso/repository/article/ArticleRepository.java
@@ -4,5 +4,4 @@ import com.cojeans.osiopso.entity.feed.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-
 }
