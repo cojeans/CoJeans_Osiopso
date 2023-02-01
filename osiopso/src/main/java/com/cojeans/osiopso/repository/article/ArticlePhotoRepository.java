dc7d4f6ce2de9faae78125f1b2139a9c8dab6a35
wonseok <bulnabang99@gmail.com>
2023-02-01 17:57:29 +0900

    Feat: PhotoRepository추가

---
+++ osiopso/src/main/java/com/cojeans/osiopso/repository/article/ArticlePhotoRepository.java
@@ -0,0 +1,7 @@
+package com.cojeans.osiopso.repository.article;
+
+import com.cojeans.osiopso.entity.feed.ArticlePhoto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticlePhotoRepository extends JpaRepository<ArticlePhoto, Long> {
+}
