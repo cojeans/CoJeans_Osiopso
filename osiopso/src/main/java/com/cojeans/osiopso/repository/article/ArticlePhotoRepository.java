c3d9f09b6015a9ed9689840b5c9727a738f4f1c8
wonseok <bulnabang99@gmail.com>
2023-02-02 17:43:03 +0900

    Feat: ArticlePhotoRepository 추가

--- osiopso/src/main/java/com/cojeans/osiopso/repository/article/ArticlePhotoRepository.java
+++ osiopso/src/main/java/com/cojeans/osiopso/repository/article/ArticlePhotoRepository.java
@@ -3,6 +3,11 @@ package com.cojeans.osiopso.repository.article;
 import com.cojeans.osiopso.entity.feed.ArticlePhoto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ArticlePhotoRepository extends JpaRepository<ArticlePhoto, Long> {
     void deleteByArticle_Id(Long articleId);
+
+    List<ArticlePhoto> findAllById(Long articleNo);
 }
