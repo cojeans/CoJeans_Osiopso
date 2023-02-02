d30161ab833a86437da9a30ad05ce1fb9df3b044
wonseok <bulnabang99@gmail.com>
2023-02-02 17:46:12 +0900

    Feat: comment pacakage 및 Repository 추가

---
+++ osiopso/src/main/java/com/cojeans/osiopso/dto/request/comment/CommentRequestDto.java
@@ -0,0 +1,17 @@
+package com.cojeans.osiopso.dto.request.comment;
+
+import com.cojeans.osiopso.entity.feed.Comment;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentRequestDto {
+    String content;
+
+}
