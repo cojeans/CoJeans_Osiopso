43ac875e9bfcc9839884d67ab7af09ebff8db7f9
wonseok <bulnabang99@gmail.com>
2023-02-02 17:40:45 +0900

    Feat: @Getter 추가

--- osiopso/src/main/java/com/cojeans/osiopso/entity/feed/Comment.java
+++ osiopso/src/main/java/com/cojeans/osiopso/entity/feed/Comment.java
@@ -2,14 +2,15 @@ package com.cojeans.osiopso.entity.feed;
 
 
 import com.cojeans.osiopso.entity.user.User;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,19 +20,21 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     @JoinColumn(name = "USER_ID")
     private User user;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     @JoinColumn(name = "ARTICLE_ID")
     private Article article;
 
     private String content;
 
+    @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
     private Date createTime;
 
+    @UpdateTimestamp
     @Temporal(TemporalType.TIMESTAMP)
     private Date modifyTime;
 
