17abaf1929b07d1ec09591477ce8106637b476ab
m5nacar5n <m5nacar5n@gmail.com>
2023-02-06 00:45:04 +0900

    Refactor: import 하나 주석처리

--- osiopso/src/main/java/com/cojeans/osiopso/service/article/ArticleService.java
+++ osiopso/src/main/java/com/cojeans/osiopso/service/article/ArticleService.java
@@ -1,7 +1,7 @@
 package com.cojeans.osiopso.service.article;
 
 import com.cojeans.osiopso.dto.request.feed.ArticlePhotoRequestDto;
-import com.cojeans.osiopso.dto.request.feed.ArticleRequestDto;
+//import com.cojeans.osiopso.dto.request.feed.ArticleRequestDto;
 import com.cojeans.osiopso.dto.tag.ArticleTagResponseDto;
 import com.cojeans.osiopso.entity.feed.Article;
 import com.cojeans.osiopso.entity.feed.ArticlePhoto;
