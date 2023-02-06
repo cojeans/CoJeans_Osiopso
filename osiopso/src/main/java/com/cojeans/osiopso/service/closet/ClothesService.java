ba52e3633720d016ade401ca13f169b6579a62d1
m5nacar5n <m5nacar5n@gmail.com>
2023-02-06 11:49:44 +0900

    Feat: 옷장 키워드 및 카테고리 검색 구현

--- osiopso/src/main/java/com/cojeans/osiopso/service/closet/ClothesService.java
+++ osiopso/src/main/java/com/cojeans/osiopso/service/closet/ClothesService.java
@@ -55,6 +55,7 @@ public class ClothesService {
 
         ClothesDto clothesDto = ClothesDto.builder()
                 .category(requestClothesDto.getCategory())
+                .originFilename(file.getName())
                 .storeFilename(file.getPath())
                 .build();
 
@@ -142,6 +143,7 @@ public class ClothesService {
     }
 
     // 3. U :  옷 정보 수정
+    // 사진 수정은 나중에 .. 따로 빼서하기
     public void editClothes(ClothesRequestDto clothesRequestDto, Long uid) {
         User user = userRepository.getById(uid);
 
