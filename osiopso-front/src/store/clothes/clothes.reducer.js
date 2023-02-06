30d857ce8a4c9ed9db37c597a895db1db25e6596
unknown <meanwo1017@gmail.com>
2023-02-06 10:00:10 +0900

    Fix: develop 브랜치 병합

---
+++ osiopso-front/src/store/clothes/clothes.reducer.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+	uploadImg:''
+}
+
+export const clothes = createSlice({
+	name: 'clothes',
+	initialState,
+	reducers: {
+		upload(state, action) {
+			state.uploadImg = action.payload;
+		}
+	}
+})
+
+export const { upload } = clothes.actions;
+export default clothes.reducer
