fb78b5bf77579e80512395cec61e9829397faf37
SSAFY <cyellowgreen@gmail.com>
2023-02-06 16:22:43 +0900

    [Front] feat: 옷 추가 페이지

--- osiopso-front/src/components/clothes-add-body/clothes-add-body.component.jsx
+++ osiopso-front/src/components/clothes-add-body/clothes-add-body.component.jsx
@@ -5,8 +5,6 @@ import ClothesAddShop from "../clothes-add-shop/clothes-add-shop.component"
 import { ClothesAddBodyContainer } from "./clothes-add-body.styles"
 
 const ClothesAddBody = ({page}) => {
-
-	
 	return (
 		<ClothesAddBodyContainer>
 			{ page ==='album'
