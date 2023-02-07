ce5f7bcc98b7b4fde55d45e8a70b0f572610d401
m5nacar5n <m5nacar5n@gmail.com>
2023-02-07 21:59:56 +0900

    Bug: cascade 오류 삭제

--- osiopso/src/main/java/com/cojeans/osiopso/entity/comment/CommentLike.java
+++ osiopso/src/main/java/com/cojeans/osiopso/entity/comment/CommentLike.java
@@ -20,15 +20,15 @@ public class CommentLike {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "COMMENT_ID")
     private Comment comment;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "USER_ID")
     private User user;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ARTICLE_ID")
     private Article article;
 }
