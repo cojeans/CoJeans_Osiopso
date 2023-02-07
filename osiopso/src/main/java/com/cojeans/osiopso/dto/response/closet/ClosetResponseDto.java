f35e1521a0b87365287f38d1f776526e06f9f696
m5nacar5n <m5nacar5n@gmail.com>
2023-02-07 15:18:21 +0900

    Refactor: 옷장 리스트 보낼 때 썸네일도 같이 보내도록 수정

--- osiopso/src/main/java/com/cojeans/osiopso/dto/response/closet/ClosetResponseDto.java
+++ osiopso/src/main/java/com/cojeans/osiopso/dto/response/closet/ClosetResponseDto.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @AllArgsConstructor
@@ -17,4 +19,6 @@ public class ClosetResponseDto {
     private Boolean isSelected;
 
     private Long count;
+
+    private List<String> thumbnails;
 }
