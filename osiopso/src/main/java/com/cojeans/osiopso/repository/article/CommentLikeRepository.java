0a589c555737d813e416a18d70a946390fe2fbe3
wonseok <bulnabang99@gmail.com>
2023-02-07 14:16:02 +0900

    Refactor: 코드 리팩토링

--- osiopso/src/main/java/com/cojeans/osiopso/repository/article/CommentLikeRepository.java
+++ osiopso/src/main/java/com/cojeans/osiopso/repository/article/CommentLikeRepository.java
@@ -1,6 +1,6 @@
 package com.cojeans.osiopso.repository.article;
 
-import com.cojeans.osiopso.entity.feed.CommentLike;
+import com.cojeans.osiopso.entity.comment.CommentLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
