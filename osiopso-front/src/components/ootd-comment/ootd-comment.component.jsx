2786fd63259c0239c3bc7b8ec6d00122a2843e62
SSAFY <youngseokgo26@naver.com>
2023-02-08 13:22:54 +0900

    Docs: ootd-component

--- osiopso-front/src/components/ootd-comment/ootd-comment.component.jsx
+++ osiopso-front/src/components/ootd-comment/ootd-comment.component.jsx
@@ -18,34 +18,60 @@ import {ReactComponent as Alert} from "../../assets/alert.svg"
 import { useState, useRef } from 'react'
 import { data } from 'dom7';
 import Swal from 'sweetalert2';
+import { useCallback } from 'react';
 
 const OotdComment=()=>{
-    const [commentData, setCommentData] = useState([])
+    const [value, setValue] = useState({
+        content:''
+    })
+    
+    const onChange = useCallback(
+        (e) => {
+            setValue(e.target.value)
+        },
+        [value]
+    )
+
+    // const onSubmit = useCallback(
+    //     e=>{
+    //         onInsert(value.name, value.content);
+    //         setValue({
+    //             content: ''
+    //         })
+    //         e.preventDefault();
+    //     },
+    //     [onInsert, value],
+    // )
+
+    
+    
+
+    
 
     const dataId = useRef(0)
     
-    const Oncreate = (content)=> {
-        const created_date = new Date().getTime();
-        const newItem= {
-            // author,
-            content,
-            created_date,
-            id: dataId.current
-        }
-        dataId.current += 1; 
-        setCommentData([newItem, ...data ])
-        content.preventDefault();
-    }
+    // const Oncreate = (content)=> {
+    //     const created_date = new Date().getTime();
+    //     const newItem= {
+    //         // author,
+    //         content,
+    //         created_date,
+    //         id: dataId.current
+    //     }
+    //     dataId.current += 1; 
+    //     setCommentData([newItem, ...data ])
+    //     content.preventDefault();
+    // }
 
-    const handleSubmit = ()=> {
-        if (commentData.content.length < 3) {
-            alert("저장 성공")
-            setCommentData({
-                // author:"",
-                content:"",
-            })                   
-        }
-    }
+    // const handleSubmit = ()=> {
+    //     if (commentData.content.length < 3) {
+    //         alert("저장 성공")
+    //         setCommentData({
+    //             // author:"",
+    //             content:"",
+    //         })                   
+    //     }
+    // }
 
     const Report = ()=>{
         Swal.fire({
@@ -87,8 +113,14 @@ const OotdComment=()=>{
       <UpperComment>
         <CommentProfileImage></CommentProfileImage>
 
-        <ClosetInput type="text" autoFocus maxLength={50}/>
-        <button onClick={Oncreate}>저장</button>
+        <ClosetInput 
+        type="text"
+        autoFocus maxLength={50} 
+        placeholder="댓글 달기..."
+        value={value.content}
+        onChange={onChange}
+        />
+        <button type="submit">저장</button>
       </UpperComment>
         </div>
     )
