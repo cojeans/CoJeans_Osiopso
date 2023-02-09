00211db71c143d9488aca4ae13c1bbda70217ab3
SSAFY <youngseokgo26@naver.com>
2023-02-09 17:34:36 +0900

    Docs: 훈수페이지 axios Post 및 Get 완료

--- osiopso-front/src/components/advice-create/advice-create.component.jsx
+++ osiopso-front/src/components/advice-create/advice-create.component.jsx
@@ -28,37 +28,48 @@ import {
 import { useBodyScrollLock } from "../../components/profile-closet/profile-closet.component"
 import Modal from '../modal/modal.component';
 
+import { ref as fref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { storage } from '../../utils/utils';
 
-const defaultOotdForm = {
+
+const defaultAdviceForm = {
   content: '',
-  picture: '',
+  imageUrl: '',
   tags :[]
 }
 const AdviceCreate = ()=> {
 
     const Token = useSelector(selectUser)
   
-    const [ootdImg, setOotdImg] = useState('')
-    const [ootdFormData, setOotdFormData] = useState(defaultOotdForm)
+    const [adviceImg, setAdviceImg] = useState('')
+    const [adviceFormData, setAdviceFormData] = useState(defaultAdviceForm)
   
-    const { content, picture, tags }= ootdFormData
+    const { content, imageUrl, tags }= adviceFormData
     const imgRef = useRef();
   
-        const saveImgFile = () => {
-          const file = imgRef.current.files[0];
-          const reader = new FileReader();
-          reader.readAsDataURL(file);
-        reader.onloadend = () => {
-          setOotdImg(reader.result)
-          setOotdFormData({...ootdFormData, picture:file})
-          };
-    };
+        const saveImgFile = async () => {
+    const file = imgRef.current.files[0];
+    const uploaded_file = await uploadBytes(
+                fref(storage, `images/${file.name}`),
+                file,
+      );
+    console.log(uploaded_file ,'testing')
+		const reader = new FileReader();
+		reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        setAdviceImg(reader.result)
+      };
+      const file_url = await getDownloadURL(uploaded_file.ref)
+      console.log(file_url)
+      setAdviceFormData({...adviceFormData, imageUrl:file_url})
+
+  };
     const handleChange = (e) => {
       const { name, value } = e.target
-      setOotdFormData({...ootdFormData, [name]:value})
+      setAdviceFormData({...adviceFormData, [name]:value})
     }
     
-    const ootdTags = useSelector(selectorOotdCategory)
+    const adviceTags = useSelector(selectorOotdCategory)
   
     const dispatch = useDispatch()
     const navigate = useNavigate()
@@ -67,11 +78,19 @@ const AdviceCreate = ()=> {
         navigate('/')
     }
   
-    const submitOotdCreate = (e) => {
+    const submitAdviceCreate = (e) => {
       e.preventDefault();
-      const formData = new FormData()
+      // const formData = new FormData()
+      const advice = {
+        tags: adviceTags,
+        content,
+        urls: [
+          {imageUrl}
+        ]
+      }
+
       const ootd = {
-        tags:ootdTags
+        tags:adviceTags
         , content
       }
       console.log(ootd)
@@ -79,20 +98,23 @@ const AdviceCreate = ()=> {
     //formdata형식의 value는 무조건 스트링으로 변환된다.
     // blob객체와 텍스트 형식 데이터만 append할 수 있는 것 같다. (File도 blob객체에 속합)
     // 그렇기에 formdata 타입으로 json타입 데이터를 보낼 때에는 blob함수로 감싸고, 두번째 인자로type: 'application/json'을 같이 넣어줘야 한다.
-      const json = JSON.stringify(ootd)
-      const blob = new Blob([json], { type: "application/json" })
+      // const json = JSON.stringify(ootd)
+      // const blob = new Blob([json], { type: "application/json" })
       
-      formData.append("picture", picture)
-      formData.append("ootd", blob)
+      // formData.append("picture", picture)
+      // formData.append("advice", blob)
   
-  
+      console.log(advice)
       axios({
         method: "post",
-        url: "http://localhost:8080/feed/ootd",
-        data: formData,
+        url: "http://localhost:8080/api/feed/advice",
+        data: {
+          tags: advice.tags,
+          content: advice.content,
+          urls: advice.urls
+        },
         headers: {
           Authorization: `Bearer ${Token.token}`,
-          "Content-Type": "multipart/form-data",
               },
       })
         .then((res) => {
@@ -101,10 +123,10 @@ const AdviceCreate = ()=> {
           console.log(err)
         })
       dispatch(resetOotdCategory())
-      AlertCreateOotd()
+      AlertCreateAdvice()
     }
   
-    const AlertCreateOotd = () => {
+    const AlertCreateAdvice = () => {
   
     Swal.fire({
       icon: 'success',
@@ -142,8 +164,8 @@ const AdviceCreate = ()=> {
             <label htmlFor="profileImg">
               <OotdImgContainer>
               {
-                ootdImg 
-                    ? <img src={ootdImg} alt="" />
+                adviceImg 
+                    ? <img src={adviceImg} alt="" />
                   : <div><span>+</span></div>
               }
               </OotdImgContainer>
@@ -171,10 +193,10 @@ const AdviceCreate = ()=> {
             >
             </textarea>
           </MarginDiv>
-          <button onClick={submitOotdCreate}>저장</button>
+          <button onClick={submitAdviceCreate}>저장</button>
         </BottomContainer>
         {
-          modalOpen && <Modal page={ false} setModalOpen={setModalOpen} openScroll={openScroll}ootdFormData={ ootdFormData} setOotdFormData = {setOotdFormData} />
+          modalOpen && <Modal page={ false} setModalOpen={setModalOpen} openScroll={openScroll}adviceFormData={ adviceFormData} setAdviceFormData = {setAdviceFormData} />
               }
       </div>
         // <div>
