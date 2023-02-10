1b0a0c55cc4ade0216d3d502a8e22e7f847db0fb
SSAFY <youngseokgo26@naver.com>
2023-02-10 12:13:50 +0900

    Docs: 훈수페이지 생성과 삭제 완료

--- osiopso-front/src/components/advice/advice.component.jsx
+++ osiopso-front/src/components/advice/advice.component.jsx
@@ -11,53 +11,48 @@ import {
 } from "./advice.styles";
 import { TextToLeft } from "../../routes/home/home.styles";
 import { useNavigate } from "react-router-dom";
-import { useSelector} from 'react-redux'
+import { useSelector } from "react-redux";
 import { ReactComponent as Comment } from "../../assets/comment.svg";
-import { selectUser } from '../../store/user/user.selector'
+import { selectUser } from "../../store/user/user.selector";
 import { VscHeart, VscComment } from "react-icons/vsc";
 
-
-import axios from 'axios'
+import axios from "axios";
 import { useEffect, useState } from "react";
 
-
-
 const Advice = () => {
-  const Token = useSelector(selectUser)
+  const Token = useSelector(selectUser);
 
-  const [adviceArticle, setAdviceArticle] = useState([])
+  const [adviceArticle, setAdviceArticle] = useState([]);
 
-  const getAdviceAxios = ()=> {
+  const getAdviceAxios = () => {
     axios({
-      method:'get',
+      method: "get",
       url: "http://localhost:8080/api/feed/advice",
       headers: {
         Authorization: `Bearer ${Token.token}`,
-      }
+      },
     })
-    .then((res)=>{
-      console.log(res)
-      setAdviceArticle(res.data.responseData.reverse())
-      console.log(res.data.responseData)
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
-  }
+      .then((res) => {
+        console.log(res.data.responseData);
+        setAdviceArticle(res.data.responseData.reverse());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  useEffect(()=>{
-    getAdviceAxios()
-  },[])
+  useEffect(() => {
+    getAdviceAxios();
+  }, []);
 
-  const goToAdviceDetail= (id)=>{
-    console.log(id)
-    navigate("detail/"+ id, {
-      state:{
-        id:id
-      }
-    })
-  }
-
+  const goToAdviceDetail = (id) => {
+    console.log(id);
+    navigate("detail/" + id, {
+      state: {
+        id: id,
+      },
+    });
+  };
 
   const navigate = useNavigate();
   const goToCheckoutHandler = () => {
@@ -66,7 +61,7 @@ const Advice = () => {
   const goToDetail = () => {
     navigate("detail");
   };
-
+  console.log("어드바이스",adviceArticle)
   return (
     <div>
       <h1>훈수페이지 입니다.</h1>
@@ -83,23 +78,20 @@ const Advice = () => {
       </TextToLeft>
 
       <HunsuImages>
-       
-          {adviceArticle.map((el,idx)=>{
-            return(
-              <div>
-              <EachBox key={idx} onClick={()=>goToAdviceDetail(el.id)}>
+        {adviceArticle.map((el, idx) => {
+          return (
+            <EachBox key={idx} onClick={() => goToAdviceDetail(el.id)}>
               <img src={el.photo.imageUrl} alt="" />
-              </EachBox>
-          <CommentContainer>
-            <VscHeart size="30" />
-            <VscComment size="30" />
-          </CommentContainer>
-          </div>
-            )
-          })}
 
-
-        
+              <CommentContainer>
+                <VscHeart size="30" />
+                <VscComment size="30" />
+                <p>{ el.time }</p>
+               
+              </CommentContainer>
+            </EachBox>
+          );
+        })}
       </HunsuImages>
     </div>
   );
