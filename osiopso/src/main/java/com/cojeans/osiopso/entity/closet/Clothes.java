5b2e61d61f29c06070b69c26a990528a85c38cf4
m5nacar5n <m5nacar5n@gmail.com>
2023-02-10 15:14:22 +0900

    Refactor: base64 테스트

--- osiopso/src/main/java/com/cojeans/osiopso/entity/closet/Clothes.java
+++ osiopso/src/main/java/com/cojeans/osiopso/entity/closet/Clothes.java
@@ -14,7 +14,10 @@ public class Clothes {
 
     private String category;
 
-    private String url;
+//  private String url;
+
+    @Column(columnDefinition = "MEDIUMBLOB")
+    private byte[] url;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "USER_ID")
