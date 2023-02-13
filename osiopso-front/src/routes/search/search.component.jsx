9a1b22a66f84fdf1ea936c5dd4003ace7f96cf3e
SSAFY <cyellowgreen@gmail.com>
2023-02-14 08:02:51 +0900

    [Front]검색

--- osiopso-front/src/routes/search/search.component.jsx
+++ osiopso-front/src/routes/search/search.component.jsx
@@ -23,7 +23,9 @@ const Search = () => {
 	const [importantHahs, setImportantHash] = useState([])
 	const navigate = useNavigate();
 	const [searchInput, setSearchInput] = useState('')
-
+	const [searchUserData, setSearchUserData] = useState([])
+	const [searchAdvice, setSearchAdvice] = useState([])
+	const [searchOotd, setSearchOotd] = useState([])
 
 	const getHash = () => {
 		setSearchInput()
@@ -31,7 +33,7 @@ const Search = () => {
 			method: 'get',
 			url: `${process.env.REACT_APP_AXIOS_URL}main/preview`,
 			headers: {
-					Authorization: `Bearer ${Token.token}`,
+				Authorization: `Bearer ${Token.token}`,
 			}
 		}).then((res) => {
 			setImportantHash(res.data.responseData)
@@ -44,44 +46,107 @@ const Search = () => {
 	const searchContent = (e) => {
 		const inputValue = e.target.value
 		console.log(inputValue)
+
+		if (!inputValue) {
+			setSearchUserData([])
+			setSearchAdvice([])
+			setSearchOotd([])
+		}
+
+		axios({
+			method: 'get',
+			url: `${process.env.REACT_APP_AXIOS_URL}feed/ootd/search/data/${inputValue}`,
+			headers: {
+				Authorization: `Bearer ${Token.token}`,
+			}
+		}).then((res) => {
+			setSearchUserData(res.data.responseData)
+			console.log('ootd/data',res.data.responseData)
+		}).catch((err) => {
+			console.log(err)
+		})
+
+
 		axios({
 			method: 'get',
 			url: `${process.env.REACT_APP_AXIOS_URL}feed/advice/search/content/${inputValue}`,
 			headers: {
-					Authorization: `Bearer ${Token.token}`,
+				Authorization: `Bearer ${Token.token}`,
 			}
 		}).then((res) => {
-			console.log(res)
+			setSearchAdvice(res.data.responseData)
+			console.log('advice/content',res.data.responseData)
+		}).catch((err) => {
+			console.log(err)
+		})
+	
+
+		axios({
+			method: 'get',
+			url: `${process.env.REACT_APP_AXIOS_URL}feed/advice/search/subject/${inputValue}`,
+			headers: {
+				Authorization: `Bearer ${Token.token}`,
+			}
+		}).then((res) => {
+			setSearchOotd(res.data.responseData)
+			console.log('ootd/content',res.data.responseData)
 		}).catch((err) => {
 			console.log(err)
 		})
 	}
-
-	useEffect(() => {
-		getHash()
-	}, [])
+			useEffect(() => {
+				getHash()
+			}, [])
 	
-	return (
-		<Fragment>
-			<SearchTop>
-				<BiArrowBack size="30" onClick={()=>navigate(-1)}/>
-				<SearchLabel>
-					<BiSearch size="25"/>
-					<SearchInput type="text" value={searchInput} onChange={searchContent} />
-				</SearchLabel>
-			</SearchTop>
 
-			<HashContainer>
-				{
-					importantHahs.map((hash, id) => {
-						return <HashBox key={id}>
-							<Circle><FaHashtag /></Circle><div className="hash">{hash.keyword}</div></HashBox>
-					})
-				}
-			</HashContainer>
 
-		</Fragment>
-	)
-}
+			return (
+				<Fragment>
+					<SearchTop>
+						<BiArrowBack size="30" onClick={() => navigate(-1)} />
+						<SearchLabel>
+							<BiSearch size="25" />
+							<SearchInput type="text" value={searchInput} onChange={searchContent} />
+						</SearchLabel>
+					</SearchTop>
+
+					<HashContainer>
+						{
+							importantHahs.map((hash, id) => {
+								return <HashBox key={id}>
+									<Circle><FaHashtag /></Circle><div className="hash">{hash.keyword}</div></HashBox>
+							})
+						}
+					</HashContainer>
+			
+					<div>
+						{
+							searchUserData.map((data, idx) => {
+								return <div key={idx}>{data.userName}</div>
+							})
+						}
+					</div>
+					<div>
+						{
+							searchAdvice.map((data, idx) => {
+								// return <img key={ idx} src={data.photo.imageUrl } alt="" />
+								return <div>{ data.photo.imageUrl}</div>
+							})
+						}
+					</div>
+					<div>
+						{
+							searchOotd.map((data, idx) => {
+								// return <img key={ idx} src={data.photo.imageUrl } alt="" />
+								return <div>{ data.photo.imageUrl}</div>
+							})
+						}
+					</div>
+
+				</Fragment>
+			)
+		}
+	
+	
 
 export default Search
