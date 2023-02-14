b0cb9443cb4b7c81e3cd16ec40c01497a3572f88
gggeon <39759666+gggeon96@users.noreply.github.com>
2023-02-14 11:29:19 +0900

    Feat: 카카오 로그인 기능

--- osiopso/src/main/java/com/cojeans/osiopso/security/oauth2/user/OAuth2UserInfoFactory.java
+++ osiopso/src/main/java/com/cojeans/osiopso/security/oauth2/user/OAuth2UserInfoFactory.java
@@ -2,19 +2,23 @@ package com.cojeans.osiopso.security.oauth2.user;
 
 import com.cojeans.osiopso.entity.user.AuthProvider;
 import com.cojeans.osiopso.exception.OAuth2AuthenticationProcessingException;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
-
+@Slf4j
 public class OAuth2UserInfoFactory {
 
     public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
+        log.info("registrationId:{}",registrationId);
         if(registrationId.equalsIgnoreCase(AuthProvider.google.toString())) {
             return new GoogleOAuth2UserInfo(attributes);
         } else if (registrationId.equalsIgnoreCase(AuthProvider.facebook.toString())) {
             return new FacebookOAuth2UserInfo(attributes);
         } else if (registrationId.equalsIgnoreCase(AuthProvider.github.toString())) {
-            return new GithubOAuth2UserInfo(attributes);
-        } else {
+            return new com.example.springsocial.security.oauth2.user.GithubOAuth2UserInfo(attributes);
+        } else if(registrationId.equalsIgnoreCase(AuthProvider.kakao.toString())){
+            return new KakaoOAuth2UserInfo(attributes);
+        }else {
             throw new OAuth2AuthenticationProcessingException("Sorry! Login with " + registrationId + " is not supported yet.");
         }
     }
