76c8bf6d6fb7a96927290d9bc2e26605c7ca7d53
wonseok <bulnabang99@gmail.com>
2023-02-14 09:32:59 +0900

    Feat: OOTD 필터링 무한스크롤 추가

--- osiopso/src/main/java/com/cojeans/osiopso/repository/article/ArticleTagQdslRepository.java
+++ osiopso/src/main/java/com/cojeans/osiopso/repository/article/ArticleTagQdslRepository.java
@@ -2,6 +2,7 @@ package com.cojeans.osiopso.repository.article;
 
 import com.cojeans.osiopso.entity.feed.Ootd;
 import com.cojeans.osiopso.entity.user.Gender;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -9,8 +10,8 @@ import java.util.List;
 public interface ArticleTagQdslRepository {
     public List<Long> findByArticleId(LocalDate now);
 
-    List<Long> findArticleByTags(List<String> styleTag, List<String> tpoTag);
-    List<Ootd> findArticleByAge(Long age);
-    List<Ootd> findArticleByGender(Gender gender);
-    List<Ootd> findArticleByAgeAndGender(Long age, Gender gender);
+    List<Long> findArticleByTags(List<String> styleTag, List<String> tpoTag, Pageable pageable, Long idx);
+    List<Ootd> findArticleByAge(Long age, Pageable pageable, Long idx);
+    List<Ootd> findArticleByGender(Gender gender, Pageable pageable, Long idx);
+    List<Ootd> findArticleByAgeAndGender(Long age, Gender gender, Pageable pageable, Long idx);
 }
