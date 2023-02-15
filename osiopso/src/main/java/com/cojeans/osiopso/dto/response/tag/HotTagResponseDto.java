659330388fb3b601f2abd66bc0f2562e6a8b8c84
m5nacar5n <m5nacar5n@gmail.com>
2023-02-15 10:44:30 +0900

    Refactor: hotTag에 첫 태그의 ootdList 함께 보내기

--- osiopso/src/main/java/com/cojeans/osiopso/dto/response/tag/HotTagResponseDto.java
+++ osiopso/src/main/java/com/cojeans/osiopso/dto/response/tag/HotTagResponseDto.java
@@ -1,11 +1,15 @@
 package com.cojeans.osiopso.dto.response.tag;
 
+import com.cojeans.osiopso.dto.response.feed.HotOotdResponseDto;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @Builder
 public class HotTagResponseDto {
     private Long id;
     private String keyword;
+    private List<HotOotdResponseDto> hotList;
 }
